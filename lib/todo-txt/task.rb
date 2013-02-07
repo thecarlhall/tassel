21ecc4418d99870ea03c7bd0040ff79412363ef7
Carl Hall <carl@hallwaytech.com>
2013-02-06 22:01:42 -0800

    Change the priority regex to allow numbers.

--- lib/todo-txt/task.rb
+++ lib/todo-txt/task.rb
@@ -3,5 +3,11 @@ module Todo
     def initialize(task)
       @orig = task.chomp
     end
+
+    # The regex used to match priorities.
+    # todo.txt only uses a-z but I wanted numbers.
+    def self.priotity_regex
+      /^\([A-Za-z0-9]\)\s+/
+    end
   end
 end
